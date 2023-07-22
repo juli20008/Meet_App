9adcebbace4b7d79c8ad05f13da8c6d2f2202762
juli20008 <mia.li.mba@gmail.com>
2023-07-22 02:17:57 -0400

    Feature2

--- src/components/Event.js
+++ src/components/Event.js
@@ -1,9 +1,32 @@
-// src/components/Event.js
+import React, { useState } from 'react';
 
-const Event = () => {
-    return (
-      <li></li>
-    );
-  }
+const Event = ({ event }) => {
+  const [showDetails, setShowDetails] = useState(false);
+
+  const handleDetailsToggle = () => {
+    setShowDetails((prevShowDetails) => !prevShowDetails);
+  };
+
+  return (
+    <div className="event">
+      <li className="location">{event.location}</li>
+      <p className="date">{event.date}</p>
+      <p className="time">{event.time}</p>
+
+      {showDetails && (
+        <div className="details-section" data-testid="details-section"> 
+          <p className="description">{event.description}</p>
+        </div>
+      )}
+
+      <button
+        className="show-details-button"
+        onClick={handleDetailsToggle}
+      >
+        {showDetails ? 'Hide Details' : 'Show Details'}
+      </button>
+    </div>
+  );
+};
 
 export default Event;
