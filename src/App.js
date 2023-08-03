2cba5fff93ec207cdd251cd7b11a02dc04a960eb
juli20008 <mia.li.mba@gmail.com>
2023-08-02 23:06:26 -0400

    4.8

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import CitySearch from './components/CitySearch';
 import EventList from './components/EventList';
 import NumberOfEvents from './components/NumberOfEvents';
 import { extractLocations, getEvents } from './api';
+import { InfoAlert, ErrorAlert } from './components/Alert';
 
 import './App.css';
 
@@ -12,6 +13,8 @@ const App = () => {
   const [currentNOE, setCurrentNOE] = useState(32);
   const [allLocations, setAllLocations] = useState([]);
   const [currentCity, setCurrentCity] = useState("See all cities");
+  const [infoAlert, setInfoAlert] = useState("");
+  const [errorAlert, setErrorAlert] = useState("");
 
   useEffect(() => {
     fetchData();
@@ -34,15 +37,23 @@ const App = () => {
 
   return (
     <div className="App">
-      <CitySearch allLocations={allLocations} setCurrentCity={setCurrentCity} />
+      <div className="alerts-container">
+        {infoAlert.length ? <InfoAlert text={infoAlert}/> : null}
+        {errorAlert.length ? <ErrorAlert text={errorAlert}/> : null}
+      </div>
+      <CitySearch allLocations={allLocations} 
+                  setCurrentCity={setCurrentCity} 
+                  setInfoAlert={setInfoAlert}/>
       <EventList events={events} numberOfEvents={currentNOE} />
       <NumberOfEvents
         id="number-of-events-component"
         numberOfEvents={currentNOE}
         onInputChange={handleInputChange}// Pass the callback function to the NumberOfEvents component
+        setErrorAlert={setErrorAlert}
       />
     </div>
   );
 }
 
 export default App;
+
