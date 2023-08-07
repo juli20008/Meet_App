eed536a787373f82ef7bf877bc7a2712cca1aa84
juli20008 <mia.li.mba@gmail.com>
2023-08-06 22:30:09 -0400

    fixfix

--- src/components/CityEventsChart.js
+++ src/components/CityEventsChart.js
@@ -20,8 +20,8 @@ const CityEventsChart = ({ allLocations, events }) => {
     const getData = () => {
         const data = allLocations.map((location) => {
         const count = events.filter((event) => event.location === location).length
-          const city = location.split(', ')[0]
-          return { city, count };
+        const city = location.split((/, | - /))[0]
+        return { city, count };
         })
         return data;
       };
@@ -31,8 +31,8 @@ const CityEventsChart = ({ allLocations, events }) => {
             margin={{
               top: 20,
               right: 20,
-              bottom: 20,
-              left: 20,
+              bottom: 60,
+              left: -30,
             }}
           >
             <CartesianGrid />
